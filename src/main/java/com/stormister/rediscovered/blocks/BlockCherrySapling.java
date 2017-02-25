8840e8e7a0d488a9f94efb91d3988c06a011964d
Dominic Amato <dominic.a.amato@gmail.com>
2017-02-25 05:19:49 -0600

    make cherry tree inherit super class

--- src/main/java/com/stormister/rediscovered/blocks/BlockCherrySapling.java
+++ src/main/java/com/stormister/rediscovered/blocks/BlockCherrySapling.java
@@ -1,54 +1,45 @@
 package com.stormister.rediscovered.blocks;
 
+import java.util.List;
 import java.util.Random;
 
-import com.stormister.rediscovered.mod_Rediscovered;
+import com.stormister.rediscovered.Rediscovered;
 import com.stormister.rediscovered.world.WorldGenCherryTrees;
 
-import net.minecraft.block.BlockBush;
-import net.minecraft.block.IGrowable;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.properties.PropertyInteger;
-import net.minecraft.block.state.BlockState;
+import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.BlockSapling;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenBigTree;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockCherrySapling extends BlockBush implements IGrowable {
-	private static final PropertyBool TYPE = PropertyBool.create("cherry");
-	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+public class BlockCherrySapling extends BlockSapling {
+	// private static final PropertyBool TYPE = PropertyBool.create("cherry");
+	// public static final PropertyInteger STAGE =
+	// PropertyInteger.create("stage", 0, 1);
+
 	private final String name = "CherrySapling";
 
 	public BlockCherrySapling() {
 		GameRegistry.registerBlock(this, name);
-		setUnlocalizedName(mod_Rediscovered.modid + "_" + name);
+		setUnlocalizedName(Rediscovered.modid + "_" + name);
 		float f = 0.4F;
 		setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f * 2.0F, 0.5F + f);
 		setCreativeTab(CreativeTabs.tabDecorations);
+		setDefaultState(blockState.getBaseState().withProperty(TYPE, BlockPlanks.EnumType.OAK).withProperty(STAGE,
+				Integer.valueOf(0)));
 	}
 
 	@Override
-	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
-		return true;
-	}
-
-	@Override
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-		return worldIn.rand.nextFloat() < 0.45D;
-	}
-
-	@Override
-	protected BlockState createBlockState() {
-		return new BlockState(this, new IProperty[] { TYPE, STAGE });
-	}
-
 	public void generateTree(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		if (!net.minecraftforge.event.terraingen.TerrainGen.saplingGrowTree(worldIn, rand, pos)) {
 			return;
@@ -65,9 +56,15 @@ public class BlockCherrySapling extends BlockBush implements IGrowable {
 		}
 	}
 
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		return 0;
+		int i = 0;
+		i = i | state.getValue(TYPE).getMetadata();
+		i = i | (state.getValue(STAGE).intValue() << 3);
+		return i;
 	}
 
 	public String getName() {
@@ -76,30 +73,17 @@ public class BlockCherrySapling extends BlockBush implements IGrowable {
 
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
-		return getDefaultState().withProperty(TYPE, false).withProperty(STAGE, Integer.valueOf((meta & 8) >> 3));
+		return getDefaultState().withProperty(TYPE, BlockPlanks.EnumType.OAK).withProperty(STAGE,
+				Integer.valueOf((meta & 8) >> 3));
 	}
 
-	public void grow(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if (state.getValue(STAGE).intValue() == 0) {
-			worldIn.setBlockState(pos, state.cycleProperty(STAGE), 4);
-		} else {
-			generateTree(worldIn, pos, state, rand);
-		}
-	}
-
+	/**
+	 * returns a list of blocks with the same ID, but different meta (eg: wood
+	 * returns 4 blocks)
+	 */
 	@Override
-	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-		this.grow(worldIn, pos, state, rand);
-	}
-
-	@Override
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if (!worldIn.isRemote) {
-			super.updateTick(worldIn, pos, state, rand);
-
-			if ((worldIn.getLightFromNeighbors(pos.up()) >= 9) && (rand.nextInt(7) == 0)) {
-				this.grow(worldIn, pos, state, rand);
-			}
-		}
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list) {
+		list.add(new ItemStack(itemIn, 1, 0));
 	}
 }
