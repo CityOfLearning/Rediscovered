16c0a87269260ae067da0d863e08b50e20ba54b5
Dominic Amato <dominic.a.amato@gmail.com>
2017-02-25 05:20:39 -0600

    fix depreciated code
    by adding anonymous irenderfactory classes

--- src/main/java/com/stormister/rediscovered/ClientProxyRediscovered.java
+++ src/main/java/com/stormister/rediscovered/ClientProxyRediscovered.java
@@ -2,11 +2,23 @@ package com.stormister.rediscovered;
 
 import com.stormister.rediscovered.blocks.tiles.TileEntityLectern;
 import com.stormister.rediscovered.blocks.tiles.TileEntityLecternOpen;
+import com.stormister.rediscovered.entity.EntityFish;
+import com.stormister.rediscovered.entity.EntityGiant;
+import com.stormister.rediscovered.entity.EntityGoodDragon;
+import com.stormister.rediscovered.entity.EntityGreenVillager;
+import com.stormister.rediscovered.entity.EntityMeleePigman;
+import com.stormister.rediscovered.entity.EntityParrow;
+import com.stormister.rediscovered.entity.EntityPigmanVillager;
+import com.stormister.rediscovered.entity.EntityRangedPigman;
+import com.stormister.rediscovered.entity.EntityRediscoveredPotion;
+import com.stormister.rediscovered.entity.EntityScarecrow;
+import com.stormister.rediscovered.entity.EntitySkeletonHorse;
+import com.stormister.rediscovered.entity.EntitySkyChicken;
+import com.stormister.rediscovered.entity.EntityZombieHorse;
 import com.stormister.rediscovered.models.ModelFish;
 import com.stormister.rediscovered.models.ModelRangedPigman;
 import com.stormister.rediscovered.models.ModelScarecrow;
 import com.stormister.rediscovered.models.ModelSkeletonHorse;
-import com.stormister.rediscovered.models.ModelSkyChicken;
 import com.stormister.rediscovered.models.ModelZombieHorse;
 import com.stormister.rediscovered.render.RenderFishMob;
 import com.stormister.rediscovered.render.RenderGiant;
@@ -17,18 +29,24 @@ import com.stormister.rediscovered.render.RenderRedDragon;
 import com.stormister.rediscovered.render.RenderRediscoveredPotion;
 import com.stormister.rediscovered.render.RenderScarecrow;
 import com.stormister.rediscovered.render.RenderSkeletonHorse;
-import com.stormister.rediscovered.render.RenderSkyChicken;
 import com.stormister.rediscovered.render.RenderTileEntityLectern;
 import com.stormister.rediscovered.render.RenderTileEntityLecternOpen;
 import com.stormister.rediscovered.render.RenderZombieHorse;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelChicken;
 import net.minecraft.client.model.ModelZombie;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderChicken;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class ClientProxyRediscovered extends CommonProxyRediscovered {
 	@Override
@@ -37,40 +55,102 @@ public class ClientProxyRediscovered extends CommonProxyRediscovered {
 
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityLecternOpen.class, new RenderTileEntityLecternOpen());
 
-		FMLCommonHandler.instance().bus().register(mod_Rediscovered.c);
+		MinecraftForge.EVENT_BUS.register(Rediscovered.config);
 
-		ModelBakery.addVariantName(mod_Rediscovered.RediscoveredPotion, "rediscovered:RediscoveredPotion_Nausea",
-				"rediscovered:RediscoveredPotion_Blindness", "rediscovered:RediscoveredPotion_Dullness",
-				"rediscovered:RediscoveredPotion_NauseaSplash", "rediscovered:RediscoveredPotion_BlindnessSplash",
-				"rediscovered:RediscoveredPotion_DullnessSplash");
+		ModelBakery.registerItemVariants(Rediscovered.RediscoveredPotion,
+				new ResourceLocation("rediscovered:RediscoveredPotion_Nausea"),
+				new ResourceLocation("rediscovered:RediscoveredPotion_Blindness"),
+				new ResourceLocation("rediscovered:RediscoveredPotion_Dullness"),
+				new ResourceLocation("rediscovered:RediscoveredPotion_NauseaSplash"),
+				new ResourceLocation("rediscovered:RediscoveredPotion_BlindnessSplash"),
+				new ResourceLocation("rediscovered:RediscoveredPotion_DullnessSplash"));
 
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityPigman.class,
-				new RenderPigman(Minecraft.getMinecraft().getRenderManager(), new ModelBiped(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityMeleePigman.class,
-				new RenderPigman(Minecraft.getMinecraft().getRenderManager(), new ModelBiped(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityRangedPigman.class,
-				new RenderPigman(Minecraft.getMinecraft().getRenderManager(), new ModelRangedPigman(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityGreenVillager.class,
-				new RenderGreenVillager(Minecraft.getMinecraft().getRenderManager()));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntitySkyChicken.class,
-				new RenderSkyChicken(Minecraft.getMinecraft().getRenderManager(), new ModelSkyChicken(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityFish.class,
-				new RenderFishMob(Minecraft.getMinecraft().getRenderManager(), new ModelFish(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityZombieHorse.class,
-				new RenderZombieHorse(Minecraft.getMinecraft().getRenderManager(), new ModelZombieHorse(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntitySkeletonHorse.class,
-				new RenderSkeletonHorse(Minecraft.getMinecraft().getRenderManager(), new ModelSkeletonHorse(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityGoodDragon.class,
-				new RenderRedDragon(Minecraft.getMinecraft().getRenderManager()));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityParrow.class,
-				new RenderParrow(Minecraft.getMinecraft().getRenderManager()));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityGiant.class,
-				new RenderGiant(Minecraft.getMinecraft().getRenderManager(), new ModelZombie(), 0.5F, 6.0F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityScarecrow.class,
-				new RenderScarecrow(Minecraft.getMinecraft().getRenderManager(), new ModelScarecrow(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(com.stormister.rediscovered.entity.EntityRediscoveredPotion.class,
-				new RenderRediscoveredPotion(Minecraft.getMinecraft().getRenderManager(),
-						Minecraft.getMinecraft().getRenderItem()));
+		RenderingRegistry.registerEntityRenderingHandler(EntityPigmanVillager.class,
+				new IRenderFactory<EntityPigmanVillager>() {
+					@Override
+					public Render<? super EntityPigmanVillager> createRenderFor(RenderManager manager) {
+						return new RenderPigman(manager, new ModelBiped(), 0.5F);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntityMeleePigman.class,
+				new IRenderFactory<EntityMeleePigman>() {
+					@Override
+					public Render<? super EntityMeleePigman> createRenderFor(RenderManager manager) {
+						return new RenderPigman(manager, new ModelBiped(), 0.5F);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntityRangedPigman.class,
+				new IRenderFactory<EntityRangedPigman>() {
+					@Override
+					public Render<? super EntityRangedPigman> createRenderFor(RenderManager manager) {
+						return new RenderPigman(manager, new ModelRangedPigman(), 0.5F);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntityGreenVillager.class,
+				new IRenderFactory<EntityGreenVillager>() {
+					@Override
+					public Render<? super EntityGreenVillager> createRenderFor(RenderManager manager) {
+						return new RenderGreenVillager(manager);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntitySkyChicken.class, new IRenderFactory<EntityChicken>() {
+			@Override
+			public Render<? super EntityChicken> createRenderFor(RenderManager manager) {
+				return new RenderChicken(manager, new ModelChicken(), 0.5F);
+			}
+		});
+		RenderingRegistry.registerEntityRenderingHandler(EntityFish.class, new IRenderFactory<EntityFish>() {
+			@Override
+			public Render<? super EntityFish> createRenderFor(RenderManager manager) {
+				return new RenderFishMob(manager, new ModelFish(), 0.5F);
+			}
+		});
+		RenderingRegistry.registerEntityRenderingHandler(EntityZombieHorse.class,
+				new IRenderFactory<EntityZombieHorse>() {
+					@Override
+					public Render<? super EntityZombieHorse> createRenderFor(RenderManager manager) {
+						return new RenderZombieHorse(manager, new ModelZombieHorse(), 0.5F);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntitySkeletonHorse.class,
+				new IRenderFactory<EntitySkeletonHorse>() {
+					@Override
+					public Render<? super EntitySkeletonHorse> createRenderFor(RenderManager manager) {
+						return new RenderSkeletonHorse(manager, new ModelSkeletonHorse(), 0.5F);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntityGoodDragon.class,
+				new IRenderFactory<EntityGoodDragon>() {
+					@Override
+					public Render<? super EntityGoodDragon> createRenderFor(RenderManager manager) {
+						return new RenderRedDragon(manager);
+					}
+				});
+		RenderingRegistry.registerEntityRenderingHandler(EntityParrow.class, new IRenderFactory<EntityParrow>() {
+			@Override
+			public Render<? super EntityParrow> createRenderFor(RenderManager manager) {
+				return new RenderParrow(manager);
+			}
+		});
+		RenderingRegistry.registerEntityRenderingHandler(EntityGiant.class, new IRenderFactory<EntityGiant>() {
+			@Override
+			public Render<? super EntityGiant> createRenderFor(RenderManager manager) {
+				return new RenderGiant(manager, new ModelZombie(), 0.5F, 6.0F);
+			}
+		});
+		RenderingRegistry.registerEntityRenderingHandler(EntityScarecrow.class, new IRenderFactory<EntityScarecrow>() {
+			@Override
+			public Render<? super EntityScarecrow> createRenderFor(RenderManager manager) {
+				return new RenderScarecrow(manager, new ModelScarecrow(), 0.5F);
+			}
+		});
+		RenderingRegistry.registerEntityRenderingHandler(EntityRediscoveredPotion.class,
+				new IRenderFactory<EntityRediscoveredPotion>() {
+					@Override
+					public Render<? super EntityRediscoveredPotion> createRenderFor(RenderManager manager) {
+						return new RenderRediscoveredPotion(manager, Minecraft.getMinecraft().getRenderItem());
+					}
+				});
 	}
 
 	@Override
